a207a897c82c210410ae3a2a4b52ea0446eb10e8
Admin <Admin@DESKTOP-7B1IAOO>
2023-03-16 15:24:24 +0530

    first commi

---
+++ Butterfly/src/com/java/school/Lkg.java
@@ -0,0 +1,11 @@
+package com.java.school;
+
+public class Lkg {
+	 public static void main(String[] args)
+	    {
+	        // Using Console to input data from user
+	        String name = System.console().readLine();
+	 
+	        System.out.println("You entered string " + name);
+	    }
+	}
